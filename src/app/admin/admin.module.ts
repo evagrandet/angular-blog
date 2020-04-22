1ca86bf6b62c372fd7e1f385303e97f5a2b4345d
evagrandet <evagrandet@ro.ru>
2020-04-22 22:18:06 +0300

    adds auth service

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
+import { AuthService } from './shared/services/auth.service';
 
 @NgModule({
     declarations: [
@@ -35,5 +37,6 @@ import { EditPageComponent } from './edit-page/edit-page.component';
         ]),
     ],
     exports: [RouterModule],
+    providers: [AuthService],
 })
 export class AdminModule {}
