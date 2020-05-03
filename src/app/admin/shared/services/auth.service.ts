04b6d909e675aab079b19eea57f01d972a854638
evagrandet <evagrandet@ro.ru>
2020-05-03 14:07:26 +0300

    adds methods for auth

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from 'src/app/shared/interfaces/user';
+import { Observable } from 'rxjs';
 @Injectable()
 export class AuthService {
     constructor(private http: HttpClient) {}
+
+    get token(): string {
+        return '';
+    }
+
+    login(user: User): Observable<any> {
+        return this.http.post('', user);
+    }
+    logout() {}
+
+    isAuthenticated(): boolean {
+        return !!this.token;
+    }
+
+    private setToken() {}
 }
