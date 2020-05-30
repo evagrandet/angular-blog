c8c50fd464d5dd651f41b3020b00e2161a5872bd
evagrandet <evagrandet@ro.ru>
2020-05-30 21:21:15 +0300

    adds form for creating page

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-create-page',
-  templateUrl: './create-page.component.html',
-  styleUrls: ['./create-page.component.scss']
+    selector: 'app-create-page',
+    templateUrl: './create-page.component.html',
+    styleUrls: ['./create-page.component.scss'],
 })
 export class CreatePageComponent implements OnInit {
+    form: FormGroup;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.form = new FormGroup({
+            title: new FormControl(null, Validators.required),
+            text: new FormControl(null, Validators.required),
+            author: new FormControl(null, Validators.required),
+        });
+    }
+    submit() {
+        if (this.form.disabled) {
+            return;
+        }
+    }
 }
