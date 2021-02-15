204a9c75e153f0271a21cb138370a8d54e740dbb
evagrandet <evagrandet@ro.ru>
2021-02-15 22:19:33 +0300

    Добавила загрузку постов

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { PostApiService } from 'src/app/shared/api/post-api.service';
+import { Post } from 'src/app/shared/interfaces/post';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
-
-  constructor() { }
+export class DashboardPageComponent implements OnInit, OnDestroy {
+  posts: Post[]
+  isLoading = false;
+  postSubscription$: Subscription;
+  constructor(private postApiService: PostApiService) { }
 
   ngOnInit() {
+    this.postSubscription$ = this.postApiService.getAllPosts().subscribe(post => {
+      this.posts = post;
+      this.isLoading = true;
+    })
+  }
+
+  deletePost(id: string) {
+
+  }
+
+  ngOnDestroy() {
+    if (this.postSubscription$) {
+      this.postSubscription$.unsubscribe()
+    }
   }
 
 }
